9ef5ba5c4d59145260ca3a8dde00efbcc1416ec6
THANH\thanh <thanhnguyen100101@gmail.com>
2023-04-17 23:22:53 +0700

    init feature

---
+++ app/src/main/java/com/st/st25nfc/generic/util/CovertActivity.java
@@ -0,0 +1,17 @@
+package com.st.st25nfc.generic.util;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.st.st25nfc.R;
+import com.st.st25nfc.generic.STFragmentActivity;
+
+public class CovertActivity extends STFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covert);
+    }
+}
